2f75e0195969d8d2a835a2d5e334a02a96242cd2
Dimitri PUYET <dimitripuyet@gmail.com>
2023-04-23 15:22:34 +0200

    add rename tv shows

---
+++ rename_tvshow.ps1
@@ -0,0 +1,20 @@
+﻿param ($path)
+
+
+########## LOCAL Variables ##########
+$path = $path + "\*"
+
+
+$movies = Get-ChildItem -Path $path -Include *.mkv,*.mp4,*.avi
+$subs = Get-ChildItem -Path $path -Include *.srt
+
+Write-Host "$($movies.count) episodes found" -ForegroundColor Green
+Write-Host "$($subs.count) subtitles found" -ForegroundColor Green
+
+
+Foreach($movie in $movies)
+{
+    $epNum = $movie.Name | Select-String -Pattern "[sS][01][0-9][eE][0-9][0-9]" -AllMatches | % { $_.Matches } | % { $_.Value }
+    $srtMatch = $subs | where {$_.name -like "*$($epnum)*"}
+    Rename-Item -LiteralPath $srtMatch.FullName -NewName $movie.Name.Replace(".mkv",".srt")
+}
